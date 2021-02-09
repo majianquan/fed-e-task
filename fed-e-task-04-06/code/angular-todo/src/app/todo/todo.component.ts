7dd1559349f27e9c96e84e6ab770bf8b821e47c1
majianquan <ma_jianquan@sina.com>
2021-02-09 22:42:46 +0800

    feat:04-06

---
+++ fed-e-task-04-06/code/angular-todo/src/app/todo/todo.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from './services/todo.service';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.sass'],
+  providers: [TodoService]
+})
+export class TodoComponent implements OnInit {
+  todoListArray: any[];
+
+  constructor(private toDoService: TodoService) { }
+
+  ngOnInit() {
+    this.toDoService.getTodoList().snapshotChanges()
+    .subscribe(item => {
+        this.todoListArray = [];
+        item.forEach(element => {
+          var x = element.payload.toJSON();
+          x["$key"] = element.key;
+          this.todoListArray.push(x);
+        });
+
+        this.todoListArray.sort((a,b) => {
+          return a.isChecked - b.isChecked;
+        });
+    });
+  }
+
+  onAdd(itemTitle) {
+    if (itemTitle.value.length) {
+      this.toDoService.addTitle(itemTitle.value);
+      itemTitle.value = null;
+    }
+  }
+
+  alterCheck($key: string, isChecked) {
+    this.toDoService.checkOrUncheckTitle($key, !isChecked);
+  }
+
+  onDelete($key: string) {
+    this.toDoService.removeTitle($key);
+  }
+
+}
